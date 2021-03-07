0edf19113f0ea29af31a777c19b78097447d94f0
pomkos <albeithome@gmail.com>
2021-03-07 19:04:11 +0000

    create airlines table

--- db_reset.py
+++ db_reset.py
@@ -3,6 +3,37 @@
 import pandas as pd
 import sqlalchemy as sq
 import datetime as dt
+import re
+
+def airline_abbrev_scrape():
+    # Read in airline abbreviations
+    cols = ['Airline', 'Country', 'IATA/ICAO Codes']
+    df = pd.DataFrame(columns = cols)
+    airlines = {}
+
+    i = 0
+    for x in pd.read_html('https://abbreviations.yourdictionary.com/articles/airline-abbreviations-for-major-carriers.html',header=0):
+        i += 1
+        try:
+            x.columns = cols # some columns had spaces in them
+        except:
+            pass
+        airlines[i] = x
+
+    for i in [1,2,3,4,5,6,7]:
+        df = df.append(airlines[i])
+
+    # Split code types
+    new = df['IATA/ICAO Codes'].str.split('/', expand = True)
+    new.columns = ['IATA', 'ICAO']
+    df = pd.concat([df, new], axis=1).drop('IATA/ICAO Codes',axis=1)
+
+    # Add Frontier Airlines, which was missing from tables
+    df = df.append({'Airline':'Frontier Airlines','Country':'USA','IATA':'F9','ICAO':'FFT'}, ignore_index=True)
+
+    # Remove spaces
+    df['IATA'] = df['IATA'].str.strip()
+    return df
 
 try:
     engine = sq.create_engine('sqlite:///data/find_me.db')
@@ -31,6 +62,10 @@ try:
         'date_added':[dt.datetime.now()]
     })
     df_ticket.to_sql('ticket_info',con=cnx,if_exists='replace',index=False)
+
+    # Reset airline abbrevs table
+    df = airline_abbrev_scrape()
+    df.to_sql('airline_info',con=cnx,index=False,if_exists='replace')
     
     print("The 'location' and 'ticket_info' tables were successfully reset!")
     
